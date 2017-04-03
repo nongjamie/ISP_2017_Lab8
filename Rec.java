6f72abef82ea655f07a29259658b7dc813030803
Kwankaew Uttama <kwankaew.u@ku.th>
2017-04-03 16:27:11 +0700

    add removeChar

--- Rec.java
+++ Rec.java
@@ -1,72 +1,73 @@
 public class Rec {
 
-
-	/** = number of the digits in the decimal representation of n.
-		e.g. numDigits(0) = 1, numDigits(3) = 1, numDigits(34) = 2.
-		numDigits(1356) = 4.
-		Precondition: n >= 0. */
-	public static int numDigits(int n) {
-
-		// fill me in
-
-	}
-
-	/** = sum of the digits in the decimal representation of n.
-      e.g. sumDigits(0) = 0, sumDigits(3) = 3, sumDigits(34) = 7,
-      sumDigits(345) = 12.
-      Precondition: n >= 0. */
-	public static int sumDigits(int num) {
-		if( num <= 0 ) {
-			return 0;
-		}
-		else if( (num > 0) && (num < 10) ) {
-			return num;
-		}
-		else{
-			return (num%10) + sumDigits( (int)Math.floor(num/10) );
-		}
-	}
-
-	/** = a copy of s with to_remove_char removed.
-		Example: removeChar("abeabe", 'e') = "abab". */
+	 /**
+	 * = number of the digits in the decimal representation of n. e.g.
+	 * numDigits(0) = 1, numDigits(3) = 1, numDigits(34) = 2. numDigits(1356)
+	 =
+	 * 4. Precondition: n >= 0.
+	 */
+	 public static int numDigits(int n) {
+	
+	 // fill me in
+	
+	 }
+	
+	 /**
+	 * = sum of the digits in the decimal representation of n. e.g.
+	 sumDigits(0)
+	 * = 0, sumDigits(3) = 3, sumDigits(34) = 7, sumDigits(345) = 12.
+	 * Precondition: n >= 0.
+	 */
+	 public static int sumDigits(int n) {
+	
+	 // fill me in
+	
+	 }
+	
+	 /**
+	 * = a copy of s with to_remove_char removed. Example:
+	 removeChar("abeabe",
+	 * 'e') = "abab".
+	 */
 	public static String removeChar(String s, char to_remove_char) {
-
-		//	 fill me in
-
-	}
-
-	/** = a copy of s with characters in reverse order.
-		Example: reverse("abcdefg") = "gfedcba". */
-	public static String reverse(String s) {
-		if( s.length() == 0 || s.equals(null) ) {
-			return "";
-		}
-		if( s.length() > 1 ) {
-			return s.substring(s.length()-1) + reverse( s.substring(0, s.length()-1)  );
-		}
-		else {
+		if (s.length() > 0) {
+			if (s.charAt(0) == to_remove_char) {
+				return removeChar(s.substring(1), to_remove_char);
+			}
+			else{
+				return s.substring(0,1).concat(removeChar(s.substring(1), to_remove_char));
+			}
+		} else
 			return s.substring(0);
-		}
+
 	}
+
+	 /**
+	 * = a copy of s with characters in reverse order. Example:
+	 * reverse("abcdefg") = "gfedcba".
+	 */
+	 public static String reverse(String s) {
+	
+	 // fill me in
+	
+	 }
 
 	public static void main(String[] args) {
-		System.out.println(numDigits(123));
-		System.out.println(numDigits(123456));
-		System.out.println(numDigits(1));
-		System.out.println(numDigits(0));
+		 System.out.println(numDigits(123));
+		 System.out.println(numDigits(123456));
+		 System.out.println(numDigits(1));
+		 System.out.println(numDigits(0));
+		 System.out.println("-----------------");
+		 System.out.println(sumDigits(123));
+		 System.out.println(sumDigits(123456));
+		 System.out.println(sumDigits(1));
+		 System.out.println(sumDigits(0));
 		System.out.println("-----------------");
-		System.out.println(sumDigits(123));
-		System.out.println(sumDigits(123456));
-		System.out.println(sumDigits(1));
-		System.out.println(sumDigits(0));
-		System.out.println("-----------------");
-		System.out.println( "a"+"b" );
-		System.out.println( removeChar( "aaabbbccc" , 'b') );
 		System.out.println(removeChar("eawabbcceccddeeaaeeeee", 'e'));
 		System.out.println(removeChar("acacacacac", 'a'));
 		System.out.println("-----------------");
-		System.out.println(reverse( "aaabbbaccabda" ));
-		System.out.println(reverse( "abbcccdeaaa" ));
-		System.out.println(reverse( "AMANAPLANACANALPANAMA" ));
+		 System.out.println(reverse("aaabbbaccabda"));
+		 System.out.println(reverse("abbcccdeaaa"));
+		 System.out.println(reverse("AMANAPLANACANALPANAMA"));
 	}
 }
